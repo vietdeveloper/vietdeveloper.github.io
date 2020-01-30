13b8d894b6e0103a98e9601e1709c7d9387f03a7
Viet <vietdeveloper@users.noreply.github.com>
2020-01-29 20:22:13 -0800

    Made getRandomBibleVerse,js

---
+++ js/getRandomBibleVerse.js
@@ -0,0 +1,83 @@
+// 2020-01-29
+var aBibles = [];
+aBibles.push("<b><i>And whatever you do, in word or in deed, do everything in the name of the Lord Jesus, giving thanks to God the Father through him. </i></b>(Colossians 3:17)");
+aBibles.push("<b><i>Blessed are those who fear the Lord and walk in his ways.</i></b> (Psalms 128:1)");
+aBibles.push("<b><i>Blessed is the man who does not walk in the counsel of the wicked, Nor stand in the way of sinners, nor sit in company with scoffers.</i></b> (Psalms 1:1)");
+aBibles.push("<b><i>Do not be afraid, Mary, for you have found favor with God.</i></b> (Luke 1:30)");
+aBibles.push("<b><i>Give thanks to the LORD of hosts, for the LORD is good; God’s love endures forever.”</i></b> (Jeremiah 33:11 NABRE)");
+aBibles.push("<b><i>He said to them, “Come after me, and I will make you fishers of men.” At once they left their nets and followed him.</i></b> (Matthew 4:19-20)");
+aBibles.push("<b><i>Jesus answered him, “Why do you call me good? No one is good but God alone.”</i></b> (Mark 10:18)");
+aBibles.push("<b><i>Jesus said to the crowd, “Take care to guard against all greed, for though one may be rich, one’s life does not consist of possessions.”</i></b> (Luke 12:15)");
+aBibles.push("<b><i>Let the peace of Christ control your hearts, the peace into which you were also called in one body. And be thankful.</i></b> (Colossians 3:15)");
+aBibles.push("<b><i>Let the peace of Christ control your hearts; let the word of Christ dwell in you richly.</i></b> (Col 3:15a, 16a");
+aBibles.push("<b><i>Let the word of Christ dwell in you richly, as in all wisdom you teach and admonish one another, singing psalms, hymns, and spiritual songs with gratitude in your hearts to God.</i></b> (Colossians 3:16)");
+aBibles.push("<b><i>Put on then, as God’s chosen ones, holy and beloved, heartfelt compassion, kindness, humility, gentleness, and patience, bearing with one another and forgiving one another, if one has a grievance against another; as the Lord has forgiven you, so must you also do.</i></b> (Colossians 3:12-13)");
+aBibles.push("<b><i>The concern of the flesh is death, but the concern of the spirit is life and peace</i></b> (Romans 8:6)");
+aBibles.push("<b><i>To God whose power now at work in us can do immeasurably more than we ask or imagine</i></b> (Ephesians 3:20)");
+aBibles.push("<b><i>“Blessed are they who hope in the Lord.”</i></b> (Ps 40:5a)");
+aBibles.push("<b><i>“Blessed the man who sets his security in the LORD, who turns not to the arrogant or to those who stray after falsehood.”</i></b> (Psalms 40:5)");
+aBibles.push("<b><i>“By no means! But I tell you, if you do not repent, you will all perish as they did!”</i></b> (Luke 13:3, NABRE)");
+aBibles.push("<b><i>“By your perseverance you will secure your lives.”</i></b> (Luke 21:19)");
+aBibles.push("<b><i>“Cast all your worries upon him because he cares for you.”</i></b> (1 Peter 5:7)");
+aBibles.push("<b><i>“Do not work for food that perishes but for the food that endures for eternal life”</i></b> (John 6:27)");
+aBibles.push("<b><i>“Do not worry about tomorrow, for tomorrow will worry about itself. Each day has enough trouble of its own.”</i></b> (Matthew 6:34)");
+aBibles.push("<b><i>“For I know well the plans I have in mind for you ― oracle of the LORD ― plans for your welfare and not for woe, so as to give you a future of hope.”</i></b> (Jeremiah 29:11)");
+aBibles.push("<b><i>“Give thanks to the LORD, who is good, whose mercy endures forever.”</i></b> (Psalms 106:1 NABRE)");
+aBibles.push("<b><i>“Give thanks to the LORD for he is good, his mercy endures forever!”</i></b> (Psalms 107:1 NABRE)");
+aBibles.push("<b><i>“Give thanks to the LORD, for he is good, his mercy endures forever.”</i></b> (Psalms 118:1 NABRE)");
+aBibles.push("<b><i>“Good indeed is the LORD, His mercy endures forever, his faithfulness lasts through every generation.”</i></b> (Psalms 100:5 NABRE)");
+aBibles.push("<b><i>“How can I repay the LORD for all the great good done for me?” </i></b> (Psalms 116:12)");
+aBibles.push("<b><i>“In all circumstances give thanks, for this is the will of God for you in Christ Jesus.”</i></b> (1 Thessalonians 5:18)");
+aBibles.push("<b><i>“In everything you do, act without grumbling or arguing; prove yourselves innocent and straightforward, children of God beyond reproach in the midst of a twisted and depraved generation—among whom you shine like the stars in the sky.”</i></b> (Philippians 2:14-15)");
+aBibles.push("<b><i>“It is better to take refuge in the LORD than to put confidence in princes.”</i></b> (Psalm 118:9, RSV-CE) ");
+aBibles.push("<b><i>“Let not your heart be troubled. You believe in God, believe also in me.” </i></b> (John 14:1. Douay-Rheims)");
+aBibles.push("<b><i>“Lord, you are merciful and gracious.”</i></b> (Psalms 86:15)");
+aBibles.push("<b><i>“Praise the LORD, for he is good; for his mercy endures forever;”</i></b> (Psalms 136:1 NABRE)");
+aBibles.push("<b><i>“Pray always without becoming weary”</i></b> (Luke 18:1, NABRE)");
+aBibles.push("<b><i>“So submit yourselves to God. Resist the devil, and he will flee from you.” </i></b> (James 4:7)");
+aBibles.push("<b><i>“They celebrate your abounding goodness and joyfully sing of your justice.”</i></b> (Psalms 145:7)");
+aBibles.push("<b><i>“Trust in God, and he will help you; make your ways straight and hope in him.”</i></b> (Sirach 2:6 NABRE)");
+aBibles.push("<b><i>“Your faith has saved you; go in peace.”</i></b> (Luke 7:50)");
+aBibles.push("Jesus answered him, “Why do you call me good? No one is good but God alone.” (Mark 10:18)");
+aBibles.push("“Blessed are they who hope in the Lord.” (Ps 40:5a)");
+aBibles.push("“Blessed the man who sets his security in the LORD, who turns not to the arrogant or to those who stray after falsehood.” (Psalms 40:5)");
+aBibles.push("“By no means! But I tell you, if you do not repent, you will all perish as they did!” (Luke 13:3)");
+aBibles.push("“By your perseverance you will secure your lives.” (Luke 21:19)");
+aBibles.push("“Cast all your worries upon him because he cares for you.” (1 Peter 5:7)");
+aBibles.push("“Do not work for food that perishes but for the food that endures for eternal life” (John 6:27)");
+aBibles.push("“Do not worry about tomorrow, for tomorrow will worry about itself. Each day has enough trouble of its own.” (Matthew 6:34)");
+aBibles.push("“For I know well the plans I have in mind for you ― oracle of the LORD ― plans for your welfare and not for woe, so as to give you a future of hope.” (Jeremiah 29:11)");
+aBibles.push("“Give thanks to the LORD for he is good, his mercy endures forever!” (Psalms 107:1, Ps 136:1)");
+aBibles.push("“I sought the LORD, and he answered me, delivered me from all my fears.” (Psalms 34:5)");
+aBibles.push("“In all circumstances give thanks, for this is the will of God for you in Christ Jesus.” (1 Thessalonians 5:18)");
+aBibles.push("“It is better to take refuge in the LORD than to put confidence in princes.” (Psalm 118:9, RSV-CE) ");
+aBibles.push("“Lord, you are merciful and gracious.” (Psalms 86:15)");
+aBibles.push("“May your mercy, LORD, be upon us; as we put our hope in you.” (Psalms 33:22, NABRE)");
+aBibles.push("“Pray always without becoming weary” (Luke 18:1, NABRE)");
+aBibles.push("“Save us by your power, and help me, who am alone and have no one but you, Lord.” (Esther C:25)");
+aBibles.push("“Trust in God, and he will help you; make your ways straight and hope in him.” (Sirach 2:6 NABRE)");
+aBibles.push("“Trust in the LORD and do good that you may dwell in the land and live secure.” (Psalms 37:3)");
+aBibles.push("<p>“<b><i>So be perfect, just as your heavenly Father is perfect.</i></b>” (Matthew 5:48)</p>");
+aBibles.push("<p>“<b><i>By no means! But I tell you, if you do not repent, you will all perish as they did!</i></b>” (Luke 13:3, NABRE)</p>");
+aBibles.push("<p>“<b><i>The concern of the flesh is death, but the concern of the spirit is life and peace</i></b>” (Romans 8:6)</p>");
+aBibles.push("<p>“<b><i>Take care to guard against all greed, for though one may be rich, one’s life does not consist of possessions.</i></b>” (Luke 12:15)</p>");
+aBibles.push("<p>“<b><i>Pray always without becoming weary.</i></b>” (Luke 18:1)</p>");
+aBibles.push("<p>“<b><i>In everything you do, act without grumbling or arguing; prove yourselves innocent and straightforward, children of God beyond reproach in the midst of a twisted and depraved generation—among whom you shine like the stars in the sky.</i></b>” (Philippians 2:14-15) </p>");
+aBibles.push("<p>“<b><i>Let us conduct ourselves properly as in the day, not in orgies and drunkenness, not in promiscuity and licentiousness, not in rivalry and jealousy.</i></b>” (Romans 13:13, NABRE)</p>");
+aBibles.push("<p>“<b><i>Ask and you will receive; seek and you will find; knock and the door will be opened to you.</i></b>” (Luke 11:9)</p>");
+aBibles.push("<p>“<b><i>No one who lights a lamp conceals it with a vessel or sets it under a bed; rather, he places it on a lampstand so that those who enter may see the light.</i></b>” (Luke 8:16)</p>");
+aBibles.push("<p>“<b><i>For we brought nothing into the world, just as we shall not be able to take anything out of it.</i></b>” (1 Timothy 6:7)</p>");
+aBibles.push("<p>“<b><i>If we have food and clothing, we shall be content with that.</i></b>” (1 Timothy 6:8)</p>");
+aBibles.push("<p>“<b><i>Those who want to be rich are falling into temptation and into a trap and into many foolish and harmful desires, which plunge them into ruin and destruction.</i></b>” (1 Timothy 6:9 )</p>");
+aBibles.push("<p>“<b><i>For the love of money is the root of all evils, and some people in their desire for it have strayed from the faith and have pierced themselves with many pains.</i></b>” (1 Timothy 6:10 )</p>");
+
+// Returns a random integer between min and max
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomBibleVerse()
+{
+    document.write(aBibles[getRandomInt(0, aBibles.length-1)]);
+}
